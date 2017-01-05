0991e10204032a995166c232b446bdf0a91b31cf
Alberto Silva <albertossilva@gmail.com>
2017-01-05 09:24:12 -0200

    Including all files in coverage

--- test/support.js
+++ test/support.js
@@ -1,4 +1,11 @@
 const chai = require('chai')
 const chaiAsPromised = require('chai-as-promised')
+const fs = require('fs')
+const path = require('path')
 
 chai.use(chaiAsPromised)
+
+const normalizedPath = path.join(__dirname, '../src')
+const requireJavascriptFile = (file) => !file.includes('.test.js') && require(`../src/${file}`)
+
+fs.readdirSync(normalizedPath).forEach(requireJavascriptFile)
